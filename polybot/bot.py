689572743ccedc70e558e40b3075cecca6d5a385
marwazu <85626810+marwazu@users.noreply.github.com>
2023-12-24 06:24:11 +0200

    Update bot.py

--- polybot/bot.py
+++ polybot/bot.py
@@ -3,6 +3,8 @@ from loguru import logger
 import os
 import time
 from telebot.types import InputFile
+import boto3
+import requests
 
 
 class Bot:
@@ -78,8 +80,53 @@ class ObjectDetectionBot(Bot):
         logger.info(f'Incoming message: {msg}')
 
         if self.is_current_msg_photo(msg):
-            pass
-            # TODO download the user photo (utilize download_user_photo)
-            # TODO upload the photo to S3
-            # TODO send a request to the `yolo5` service for prediction
-            # TODO send results to the Telegram end-user
+            try:
+                #Download the user photo
+                image_path=self.download_user_photo(msg)
+                image_name=image_path.split('/')[-1]
+
+                #upload the photo to S3
+                images_bucket = os.environ['BUCKET_NAME']
+                s3 = boto3.client("s3")
+                s3.upload_file(image_path, images_bucket, image_name)
+                logger.info(f' {image_name} : Uploading img to s3 completed')
+            except Exception as e:
+                logger.error(f'Failed to upload {image_name} to S3: {e}')
+                return
+
+            try:
+                #send a request to the `yolo5` service for prediction
+                response = requests.post('http://localhost:8081/predict', params={'imgName': image_name})
+                if response.status_code != 200:
+                    logger.error(f'the prediction request to yolo5 service failed')
+                else:
+                    #send results to the Telegram end-user
+                    predictions = response.json()
+                    logger.info(f'Received predictions: {predictions}')
+                    predictionsformat= self.predictions_format(predictions)
+                    try:
+                        self.send_text(msg['chat']['id'], predictionsformat)
+                    except Exception as e:
+                        logger.error(f'Failed to send predictions to user: {e}')
+            except Exception as e:
+                logger.error(f' Requesting predictions from yolo5 service failed !: {e}')
+
+
+    # find the detected objects and count them in a formatted way
+    def predictions_format(self, predictions):
+        try:
+            labels = predictions.get('labels', [])
+            detected_objects = {}
+            for label in labels:
+                objectDetected = label['class']
+                if objectDetected in detected_objects:
+                    detected_objects[objectDetected] +=1
+                else :
+                    detected_objects[objectDetected]=1
+            for object, count in detected_objects.items():
+                result += f"{object}: {count}\n"
+            return "Detected objects:\n"+result
+
+        except Exception as e:
+            logger.error(f'prediction format failed: {e}')
+            return "Error in predictions_format"
